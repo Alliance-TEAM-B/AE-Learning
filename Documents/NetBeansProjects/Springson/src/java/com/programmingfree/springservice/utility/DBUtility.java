1c40feeeaed610396dc592830ad105876f3f66ab
Siegfred7 <siegfred.rodriguez@gmail.com>
2015-05-15 09:00:56 +0800

    Spring JSON

---
+++ Documents/NetBeansProjects/Springson/src/java/com/programmingfree/springservice/utility/DBUtility.java
@@ -0,0 +1,53 @@
+package com.programmingfree.springservice.utility;
+/*
+execute :
+    CREATE TABLE tblUser(
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`userid`)
+)
+
+*/
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBUtility {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection != null) {
+            return connection;
+        } else {
+            try {
+                Properties prop = new Properties();
+                InputStream inputStream = DBUtility.class.getClassLoader().getResourceAsStream("config.properties");
+                prop.load(inputStream);
+                String driver = prop.getProperty("driver");
+                String url = prop.getProperty("url");
+                String user = prop.getProperty("user");
+                String password = prop.getProperty("password");
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
+
+    }
+
+}
